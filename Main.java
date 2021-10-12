9f3b61c3941f587cb18e6736218fa75636d330fa
PDHAKAL PRABESH <c210103@ccg.ac.jp>
2021-10-12 11:20:16 +0900

    「test」の表示

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+import javax.print.event.PrintJobListener;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Japan!");
         System.out.println("Hello Java!");
+    
+        System.out.println("Test");
     }
 }
